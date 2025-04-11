39e9d085572cde66b30dc5bafa73a4080c825632
rojabadavath28 <rojabadavathroja318@gmail.com>
2025-04-10 22:04:14 -0700

    Add files via upload

---
+++ number of rows.py
@@ -0,0 +1,9 @@
+#number of rows
+rows=6
+#outer loop to handle the number of rows
+for i in range(rows , 0,-1):
+    #inner loop to print the repeated number in each row
+    for j in range(rows-i+1):
+     print(i,end=" ")
+    #move to the next line after each row
+    print()
